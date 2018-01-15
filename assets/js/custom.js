34f29b1b69e090d37e6468873b7a7a482ba841e6
Aryak Sengupta <aryak.sengupta@gmail.com>
2018-01-15 12:40:04 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,7 +21,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2018/04/29').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2018/04/30').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
